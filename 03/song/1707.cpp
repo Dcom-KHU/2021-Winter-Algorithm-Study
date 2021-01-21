2a8d7df00e83e2fb54b123d6446cbc7c8488cfdc
JaeHyeok Song <72161388+JaehyeokSong0@users.noreply.github.com>
2021-01-22 00:12:03 +0900

    fixed error of 1707

--- 03/song/1707.cpp
+++ 03/song/1707.cpp
@@ -1,25 +1,31 @@
-//이 코드는 알고리즘 상으로는 맞으나 백준에서 돌려보면 메모리 초과로 실패가 뜹니다..
-//인접 리스트로 바꾸면 해결된다고는 하는데 시간 문제로 못 해결했습니다 ㅎ
-//빠른 시간안에 수정해서 올리겠습니다 ㅎㅎ
-
 #include <iostream>
+#include <vector>
+#include <memory.h> //memset() 사용 시 vs에서는 없어도 작동되나, 백준에서는 이 헤더가 있어야 사용가능
 using namespace std;
 
 #define RED 1
 #define BLUE 2
 
 int v, e; //v:정점의 개수, e:간선의 개수
-int check[20001] = { 0, }; //방문하지 않았을 경우 0, 색깔은 red&blue로 표시
-bool adjacentMatrix[20001][20001] = { false, };
+int color[20001] = { 0, }; //방문하지 않았을 경우 0, 색깔은 red&blue로 표시
+vector<int> adjacentList[20001];
+
+//테스트케이스마다 color 및 인접행렬 초기화
+void clearList()
+{
+	memset(color, 0, sizeof(color));
+	for (int i = 0; i < 20001; i++)
+		adjacentList[i].clear();
+}
 
 //dfs 진행하며 check한 정점에 색깔 기록
-void setColor(int x, int color)
+void setColor(int x, int _color)
 {
-	check[x] = color;
-	for (int i = 1; i <= v; i++)
+	color[x] = _color;
+	for (int i = 0; i < adjacentList[x].size(); i++)
 	{
-		if (adjacentMatrix[x][i] && check[i] == 0)
-			setColor(i, 3 - color); //반대 색깔 인자로 주어 함수 실행
+		if (color[adjacentList[x][i]] == 0)
+			setColor(adjacentList[x][i], 3 - _color); //반대 색깔 인자로 주어 함수 실행
 	}
 }
 
@@ -28,13 +34,10 @@ bool isBipartite()
 {
 	for (int i = 1; i <= v; i++)
 	{
-		for (int j = 1; j <= v; j++)
+		for (int j = 0; j < adjacentList[i].size(); j++)
 		{
-			if (adjacentMatrix[i][j] == true) //인접 정점에 대해서
-			{
-				if (check[i] == check[j]) //서로 색깔이 같으면
-					return false; //이분그래프가 아님
-			}
+			if (color[i] == color[adjacentList[i][j]]) //서로 색깔이 같으면
+				return false; //이분그래프가 아님
 		}
 	}
 	return true;
@@ -50,16 +53,22 @@ int main()
 	for (int i = 0; i < k; i++)
 	{
 		cin >> v >> e;
-		
-		//인접행렬 초기화
+
+		//인접리스트 초기화
+		clearList();
 		for (int j = 0; j < e; j++)
 		{
 			int v1, v2;
 			cin >> v1 >> v2;
-			adjacentMatrix[v1][v2] = adjacentMatrix[v2][v1] = true;
+			adjacentList[v1].push_back(v2);
+			adjacentList[v2].push_back(v1);
 		}
 
-		setColor(1,RED);
+		for (int i = 1; i <= v; i++) //분리되어있는 정점이 있을 수 있으므로 반복문 통해 color 설정
+		{
+			if (color[i] == 0)
+				setColor(i, RED);
+		}
 		if (isBipartite())
 			cout << "YES" << endl;
 		else
