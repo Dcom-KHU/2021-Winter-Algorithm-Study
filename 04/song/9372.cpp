bdb57a142f1b7f634d42095d1b11ed95be197d20
JaehyeokSong0 <jaehyeok.song0@gmail.com>
2021-01-26 02:11:20 +0900

    improve time and memory performance

--- 04/song/9372.cpp
+++ 04/song/9372.cpp
@@ -1,20 +1,20 @@
-#include <iostream>
+#include <stdio.h>
 using namespace std;
 
 int main()
 {
     int t,n,m; //t:테스트 케이스의 수, n:국가의 수, m:비행기의 종류
-    cin>>t;
+    scanf("%d",&t);
     for(int i=0;i<t;i++)
     {
-        cin>>n>>m;
+        scanf("%d %d",&n,&m);
         for(int j=0;j<m;j++)
         {
             int tmp1, tmp2;
-            cin>>tmp1>>tmp2; //간선정보 사용X
+            scanf("%d %d",&tmp1,&tmp2); //간선정보 사용X
         }
         //연결 그래프이므로 모든 정점 간 경로가 존재 -> 간선의 최소 개수는 (정점의 개수-1) 
-        cout<<n-1<<endl; 
+        printf("%d\n",n-1); 
     }
     return 0;
 }
